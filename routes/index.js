a3f5aa927d5e776b9e3d8ab1bad9bd62ee918a84
Chandan y <chandan.yadav@datatobiz.com>
2024-02-04 17:31:34 +0530

    doc removed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger-output.json");
 
 router.use('/location', require('./location.js'));
 router.use('/weather', require('./weather.js'));
 router.use('/history', require('./history.js'));
-router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
